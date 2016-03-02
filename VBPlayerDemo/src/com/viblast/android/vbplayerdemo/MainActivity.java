950caf726430ce9e16c220a39127e12242eaf219
Plamen Tanov <plamen.tanov@viblast.com>
2016-03-02 13:28:14 +0200

    #5 Support client ID when licensing

--- VBPlayerDemo/src/com/viblast/android/vbplayerdemo/MainActivity.java
+++ VBPlayerDemo/src/com/viblast/android/vbplayerdemo/MainActivity.java
@@ -22,12 +22,14 @@ public class MainActivity extends Activity {
 		// start faster, using the lowest resolution:
 		vbConfig.advancedConfig.put("initial-abr-index", "0");
 
-		vbConfig.setCdnStream("https://nimble.viblast.com:8081/live/abr.smil/playlist.m3u8");
+		vbConfig.setCdnStream("http://cdn3.viblast.com/streams/hls/airshow/playlist.m3u8");
 		
 		vbConfig.advancedConfig.put("enable-pdn", "true");
 		vbConfig.advancedConfig.put("enable-realtime-loggger", "true");
 		vbConfig.advancedConfig.put("realtime-logger-server", "wss://cs.viblast.com/rt");
-		vbConfig.advancedConfig.put("key", "200057d28abdc9fb593eb654629f2f03c14fac9c5fc0825c899bd6095ad7a8de79ad770b4e99ec1581285ecb2cac1d6d");
+		vbConfig.advancedConfig.put("key", "200057d28abdc9fb593eb654629f2f03c14fac9c5fc0825c899bd6095ad7a8de5f83d52297310d340876edc852425419de9630208b8476a4307ad1082f80f033c8ffb5d5100fdf126e25ff7c77759cf1");
+//		if you want to see Viblast logs:
+//		vbConfig.advancedConfig.put("log", "verbose");
 
 		viblastPlayer = new ViblastPlayer(viblastView, vbConfig);
 	}
